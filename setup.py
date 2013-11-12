dede0743781fb3052708ad6b2c1b213cfefb6496
Chet Luther <chet.luther@gmail.com>
2013-11-12 15:56:01 -0600

    Version 1.1.6 -> 1.2.0dev.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.OpenStack"
-VERSION = "1.1.6"
+VERSION = "1.2.0dev"
 AUTHOR = "Zenoss"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
