39cd8794197014deb199ee4629b5daed1f55e86a
yichi-lu <teegle@yahoo.com>
2014-08-05 16:37:20 -0500

    ZEN-13131: twisted keystone

--- ZenPacks/zenoss/OpenStack/apiclients/keystoneapiclient.py
+++ ZenPacks/zenoss/OpenStack/apiclients/keystoneapiclient.py
@@ -12,11 +12,15 @@
 #
 ###########################################################################
 
+import Globals
+
 from ZenPacks.zenoss.OpenStack.utils import add_local_lib_path
 add_local_lib_path()
 
 from keystoneclient.v2_0.client import Client as keystoneclient
 
+from twisted.internet.defer import inlineCallbacks
+from twisted.internet import reactor, defer, threads
 
 class KeystoneAPIClient(object):
 
@@ -26,30 +30,142 @@ class KeystoneAPIClient(object):
         self._project_id = project_id
         self._auth_url = url
         # use keystone client v2 by default
+        self._client = None
+
+        if reactor.running == False:
+            reactor.run()
+
+    def __get_client(self):
         self._client = keystoneclient(
             username=self._username,
             password=self._api_key,
             tenant_name=self._project_id,
             auth_url=self._auth_url,
         )
-        self._token = self._get_token()
 
     def _get_token(self):
-        # token expires in 3600 seconds
+        self.__get_client()
         return self._client.service_catalog.catalog['token']['id']
 
-    def get_endpoints(self):
+    @inlineCallbacks
+    def get_token(self):
+        result = []
+        try:
+            result = yield self._get_token()
+        except Exception as err:
+            print "Keystone API Client get_token() error: ", err
+
+        defer.returnValue(result)
+
+    def _get_endpoints(self):
+        self.__get_client()
         return self._client.endpoints.list()
 
-    def get_roles(self):
+    @inlineCallbacks
+    def get_endpoints(self):
+        result = []
+        try:
+            result = yield self._get_endpoints()
+        except Exception as err:
+            print "Keystone API Client get_endpoints() error: ", err
+
+        defer.returnValue(result)
+
+    def _get_regions(self):
+        self.__get_client()
+        regions = set()
+        endpoints = self._client.service_catalog.get_endpoints()
+        for (service, service_endpoints) in endpoints.iteritems():
+            for endpoint in service_endpoints:
+                regions.add(endpoint['region'])
+
+        return [{'key': c, 'label': c} for c in sorted(regions)]
+
+    @inlineCallbacks
+    def get_regions(self):
+        result = []
+        try:
+            result = yield self._get_regions()
+        except Exception as err:
+            print "Keystone API Client get_regions() error: ", err
+
+        defer.returnValue(result)
+
+    def _get_roles(self):
+        self.__get_client()
         return self._client.roles.list()
 
-    def get_services(self):
+    @inlineCallbacks
+    def get_roles(self):
+        result = []
+        try:
+            result = yield self._get_roles()
+        except Exception as err:
+            print "Keystone API Client get_roles() error: ", err
+
+        defer.returnValue(result)
+
+    def _get_services(self):
+        self.__get_client()
         return self._client.services.list()
 
-    def get_tenants(self):
+    @inlineCallbacks
+    def get_services(self):
+        result = []
+        try:
+            result = yield self._get_services()
+        except Exception as err:
+            print "Keystone API Client get_services() error: ", err
+
+        defer.returnValue(result)
+
+    def _get_tenants(self):
+        self.__get_client()
         return self._client.tenants.list()
 
-    def get_users(self):
+    @inlineCallbacks
+    def get_tenants(self):
+        result = []
+        try:
+            result = yield self._get_tenants()
+        except Exception as err:
+            print "Keystone API Client get_tenants() error: ", err
+
+        defer.returnValue(result)
+
+    def _get_ceilometerurl(self, region_name):
+        self.__get_client()
+        endpoints = self._client.service_catalog.get_endpoints('metering')
+        if 'metering' in endpoints:
+             for endpoint in endpoints['metering']:
+                 if endpoint.has_key('region') and \
+                    endpoint['region'] == region_name and \
+                    endpoint.has_key('publicURL'):
+                     return endpoint['publicURL']
+        return ""
+
+    @inlineCallbacks
+    def get_ceilometerurl(self, region_name = ""):
+        result = []
+        if len(region_name) > 0:
+            try:
+                result = yield self._get_ceilometerurl(region_name)
+            except Exception as err:
+                print "Keystone API Client get_ceilometerurl() error: ", err
+
+        defer.returnValue(result)
+
+    def _get_users(self):
+        self.__get_client()
         return self._client.users.list()
+
+    @inlineCallbacks
+    def get_users(self):
+        result = []
+        try:
+            result = yield self._get_users()
+        except Exception as err:
+            print "Keystone API Client get_users() error: ", err
+
+        defer.returnValue(result)
 
