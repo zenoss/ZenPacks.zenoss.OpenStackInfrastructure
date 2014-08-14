6363231231965e1a49f8ddcb157c6d3c400da8ad
yichi-lu <teegle@yahoo.com>
2014-08-14 02:19:05 -0500

    temp commit

---
+++ ZenPacks/zenoss/OpenStack/apiclients/novaapiclient.py
@@ -0,0 +1,345 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2013, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+"""novaapiclient - Client library for the OpenStack Nova API.
+
+Example usage:
+
+    >>> c = novaapiclient.Client(username, password, auth_url, project_id)
+    >>> c.flavors()
+    {
+        "flavors": [list of flavors ],
+    }
+
+"""
+
+import collections
+import datetime
+import httplib
+import json
+import pdb
+
+from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.web import client as txwebclient
+from twisted.web.error import Error
+from twisted.internet import reactor
+
+
+__all__ = [
+    'Client',
+
+    # Exceptions
+    'NovaError',
+    'BadRequestError',
+    'UnauthorizedError',
+    ]
+
+
+USER_AGENT = 'python-novaclient'
+
+
+Request = collections.namedtuple(
+    'Request', ['url', 'method', 'agent', 'headers', 'postdata'])
+
+
+def getPageAndHeaders(url, contextFactory=None, *args, **kwargs):
+    """Return deferred with a (body, headers) success result.
+
+    This is a small modification to twisted.web.client.getPage that
+    allows the caller access to the response headers.
+
+    """
+    factory = txwebclient._makeGetterFactory(
+        url,
+        txwebclient.HTTPClientFactory,
+        contextFactory=contextFactory,
+        *args, **kwargs)
+
+    return factory.deferred.addCallback(
+        lambda page: (page, factory.response_headers))
+
+
+class NovaAPIClient(object):
+
+    """Twisted asynchronous Nova client."""
+
+    def __init__(self, username, password, auth_url, project_id, region_name):
+        """Create Nova API client."""
+        self.username = username
+        self.password = password
+        self.auth_url = auth_url
+        self.project_id = project_id
+        self.region_name = region_name
+
+        self._api = None
+        self._management_url = None
+        self._token = None
+        self._service_catalog = None
+
+
+    @property
+    def flavors(self):
+        """Return entry-point to the API."""
+        if not self._api:
+            self._api = API(self, '/flavors/detail')
+
+        return self._api
+
+    @property
+    def roles(self):
+        """Return entry-point to the API."""
+        if not self._api:
+            self._api = API(self, '/OS-KSADM/roles')
+
+        return self._api
+
+    @property
+    def services(self):
+        """Return entry-point to the API."""
+        if not self._api:
+            self._api = API(self, '/OS-KSADM/services')
+
+        return self._api
+
+    @property
+    def tenants(self):
+        """Return entry-point to the API."""
+        if not self._api:
+            self._api = API(self, '/tenants')
+
+        return self._api
+
+    @property
+    def users(self):
+        """Return entry-point to the API."""
+        if not self._api:
+            self._api = API(self, '/users')
+
+        return self._api
+
+    def __getitem__(self, name):
+        if name == 'flavors':
+            return self.flavors
+        elif name == 'roles':
+            return self.roles
+        elif name == 'services':
+            return self.services
+        elif name == 'tenants':
+            return self.tenants
+        elif name == 'users':
+            return self.users
+
+        raise TypeError(
+            "%r object is not subscriptable (except for flavors" + \
+            ", roles, services, tenants, users",
+            self.__class__.__name__)
+
+    @inlineCallbacks
+    def login(self):
+        """Login to Nova.
+
+        Client normally handles logins transparently. So it's not
+        recommended that login be explicitely called for most usages.
+
+        """
+        if self._token:
+            returnValue(None)
+
+        body = {}
+        body["auth"] = {}
+        body["auth"]["tenantName"] = self.project_id
+        body["auth"]["passwordCredentials"] = {}
+        body["auth"]["passwordCredentials"]["username"] = self.username
+        body["auth"]["passwordCredentials"]["password"] = self.password
+
+        r = yield self.direct_api_call('/tokens', data=body)
+        # pdb.set_trace()
+        self._token = r['access']['token']['id'].encode('ascii','ignore')
+        # print "r access: %s" % str(r['access'])
+        # print "token: %s" % self._token
+        self._service_catalog = r['access']['serviceCatalog']
+        for sc in r['access']['serviceCatalog']:
+            if sc['endpoints'][0]['adminURL'].find(':35357') > -1:
+                self._management_url = sc['endpoints'][0]['adminURL'].encode('ascii','ignore')
+        # print "management_url: %s" % self._management_url
+        returnValue(r)
+
+    @inlineCallbacks
+    def api_call(self, path, data=None, params=None, **kwargs):
+        """Wrap direct_api_call for convenience purposes.
+
+        Will authenticate prior to making an API call if necessary, and
+        reauthenticate and retry the API call if an unauthorized error
+        is encountered during the first API call attempt.
+
+        """
+        if not self._token:
+            yield self.login()
+
+        try:
+            r = yield self.direct_api_call(
+                path, data=data, params=params, **kwargs)
+        except UnauthorizedError:
+            # Could be caused by expired token. Try to login.
+            yield self.login()
+
+            # Then try the call again.
+            r = yield self.direct_pi_call(
+                path, data=data, params=params, **kwargs)
+
+        returnValue(r)
+
+    @inlineCallbacks
+    def direct_api_call(self, path, data=None, params=None, **kwargs):
+        """Return result of Nova request.
+
+        Typically this is not meant to be called directly. It is meant
+        to be used through the api property as follows:
+
+            client.flavors({'data': 'value'})
+
+        However, it can be used directly as follows:
+
+            client.api_call('/flavors', data={'data': 'value'})
+
+        """
+        pdb.set_trace()
+        request = self._get_request(path, data=data, params=params, **kwargs)
+
+        try:
+            response = yield getPageAndHeaders(
+                request.url,
+                method=request.method,
+                agent=request.agent,
+                headers=request.headers,
+                postdata=request.postdata)
+
+        except Error as e:
+            status = int(e.status)
+            response = json.loads(e.response)
+            text = response['error']['message']
+
+            if status == httplib.UNAUTHORIZED:
+                raise UnauthorizedError(text)
+            elif status == httplib.BAD_REQUEST:
+                raise BadRequestError(text)
+            elif status == httplib.NOT_FOUND:
+                raise NotFoundError(text)
+
+            raise NovaError(text)
+
+        except Exception as e:
+            raise NovaError(e)
+
+        returnValue(json.loads(response[0]))
+
+    def _get_request(self, path, data=None, params=None, **kwargs):
+        # Process data. Supports a variety of input types.
+        # /tokens uses POST; all others use GET
+        postdata = json.dumps(data) if data else None
+
+        method = 'POST' if postdata else 'GET'
+
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'User-Agent': USER_AGENT,
+            }
+
+        if self._token:
+            headers['X-Auth-Token'] = self._token
+
+        if self._management_url is not None and method == 'GET':
+            auth_url = self._management_url
+        else:
+            auth_url = self.auth_url
+        return Request(
+            auth_url + path,
+            method=method,
+            agent=USER_AGENT,
+            headers=headers,
+            postdata=postdata)
+
+
+    def callback(self, result):
+        print "result: %s" % result
+        reactor.stop()
+
+    def errback(self, failure):
+        print failure.getErrorMessage()
+        reactor.stop()
+
+
+class API(object):
+
+    """Wrapper for each element of an API path including the leaf.  """
+
+    def __init__(self, client, path=''):
+        self.client = client
+        self.path = path
+        self._apis = {}
+
+    def __getattr__(self, name):
+        if name not in self._apis:
+            self._apis[name] = API(self.client, '/'.join((self.path, name)))
+
+        return self._apis[name]
+
+    def __getitem__(self, name):
+        return getattr(self, name)
+
+    def __call__(self, data=None, params=None, **kwargs):
+        return self.client.api_call(
+            self.path, data=data, params=params, **kwargs)
+
+
+## Exceptions #########################################################
+
+class NovaError(Exception):
+
+    """Parent class of all exceptions raised by txciscoapic."""
+
+    pass
+
+
+class BadRequestError(NovaError):
+
+    """Wrapper for HTTP 400 Bad Request error."""
+
+    pass
+
+
+class UnauthorizedError(NovaError):
+
+    """Wrapper for HTTP 401 Unauthorized error."""
+
+    pass
+
+
+class NotFoundError(NovaError):
+
+    """Wrapper for HTTP 400 Bad Request error."""
+
+    pass
+
+
+def main():
+    c = NovaAPIClient('admin', 'password', 'http://192.168.56.104:5000/v2.0', 'demo', 'RegionOne')
+#   c = NovaAPIClient('admin', '8a041d9c59dd403a', 'http://10.87.208.184:5000/v2.0', 'admin')
+    ret = c.flavors()
+#   ret = c.roles()
+#   ret = c.services()
+#   ret = c.tenants()
+#   ret = c.users()
+    reactor.run()
+    print ret.result
+
+if __name__ == '__main__':
+    main()
+
