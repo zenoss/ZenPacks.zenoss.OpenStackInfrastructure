1265b31fedaed4876b20890d9c6cb6a375b356bb
Josh Wilmes <jwilmes@zenoss.com>
2014-08-19 16:43:10 -0400

    Update copyright dates.

--- ZenPacks/zenoss/OpenStack/apiclients/novaapiclient.py
+++ ZenPacks/zenoss/OpenStack/apiclients/novaapiclient.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (C) Zenoss, Inc. 2013, all rights reserved.
+# Copyright (C) Zenoss, Inc. 2014, all rights reserved.
 #
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
