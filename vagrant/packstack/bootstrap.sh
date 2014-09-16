9b74ee80fae975fb176c0098cb6d4191336b77c7
Josh Wilmes <jwilmes@zenoss.com>
2014-09-16 18:22:37 +0000

    Fix for errors about concurrent ssh sessions.  Due to the number of command plugins, it tries to open a bunch of ssh conncetions.. just bump up the limit for now.

--- vagrant/packstack/bootstrap.sh
+++ vagrant/packstack/bootstrap.sh
@@ -50,6 +50,10 @@ for e in openstack-ceilometer-alarm-evaluator openstack-ceilometer-alarm-notifie
 # Create a zenoss user so that zenoss can monitor this host as well.
 adduser zenoss -p '$6$GBeC9/Vf$0/6klsM6XThSI/nXvZTwsn1ESPjKjSbmlXj1Okh1i2CVTCknekldztlvhAF5ki85a94FejZ1cliKd30Met0BT/'
 
+# Increase MaxSessions for sshd:
+perl -p -i -e 's/#MaxSessions 10/MaxSessions 100/g' /etc/ssh/sshd_config
+/etc/init.d/sshd restart
+
 # set up the vagrant user with the right openstack environment so you can just
 # log in and run the openstack commandline tools.
 cp /root/keystonerc_admin /home/vagrant/keystonerc_admin
