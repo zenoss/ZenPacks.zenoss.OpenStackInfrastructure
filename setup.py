5199a571380726c5d20b9b4a1de53c25dc4fb644
Josh Wilmes <jwilmes@zenoss.com>
2014-10-09 22:15:15 +0000

    post release: 2.0.0 -> 2.0.1dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.OpenStackInfrastructure"
-VERSION = "2.0.0"
+VERSION = "2.0.1dev"
 AUTHOR = "Zenoss"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = [u'ZenPacks', u'ZenPacks.zenoss']
