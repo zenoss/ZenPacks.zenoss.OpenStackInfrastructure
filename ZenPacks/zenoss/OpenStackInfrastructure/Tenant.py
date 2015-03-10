c99e37e269f2ed38cbc562beb4d333f0219deec2
Philip Carinhas <pcarinhas@zenoss.com>
2015-03-10 13:55:07 -0500

    Filter device components

--- ZenPacks/zenoss/OpenStackInfrastructure/Tenant.py
+++ ZenPacks/zenoss/OpenStackInfrastructure/Tenant.py
@@ -16,8 +16,24 @@ LOG = logging.getLogger('zen.OpenStackInfrastructureTenant')
 class Tenant(schema.Tenant):
 
     def tenant_impacted_by(self):
+
+        impacted_list = []
+        dev = self.device()
+        t_types = ('OpenStackInfrastructureInstance',
+                   'OpenStackInfrastructureNetwork',
+                   'OpenStackInfrastructureSubnet',
+                   )
+
+        # Build raw list of all components first
         comps = []
-        comps = self.getDeviceComponents(type="OpenStackInfrastructureInstance")
-        comps += self.getDeviceComponents(type="OpenStackInfrastructureNetwork")
-        comps += self.getDeviceComponents(type="OpenStackInfrastructureSubnet")
-        return comps
+        for t in t_types:
+            comps.extend(dev.getDeviceComponents(type=t))
+
+        # Filter the raw list
+        impacted_list = [c for c in comps
+                         if hasattr(c, 'tenant')
+                         if c.tenant()
+                         if self.id == c.tenant().id
+                         ]
+
+        return impacted_list
