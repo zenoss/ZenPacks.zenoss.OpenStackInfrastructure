3feb19d84df62afdbed9e6683dfad4c102ee10a3
Philip Carinhas <pcarinhas@zenoss.com>
2015-03-27 12:56:36 -0500

    Change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.OpenStackInfrastructure"
-VERSION = "2.0.1dev"
+VERSION = "2.1.0dev"
 AUTHOR = "Zenoss"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = [u'ZenPacks', u'ZenPacks.zenoss']
