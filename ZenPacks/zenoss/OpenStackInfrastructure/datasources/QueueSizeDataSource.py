6c53d82a28f1cc2c13cc76e1a11abfe54669e9da
Josh Wilmes <jwilmes@zenoss.com>
2015-05-15 17:01:06 -0400

    Handle AMQP connections more carefully, and catch an exception that was being thrown by non-existent queues (and thus causing reconnects and leaking connections

--- ZenPacks/zenoss/OpenStackInfrastructure/datasources/QueueSizeDataSource.py
+++ ZenPacks/zenoss/OpenStackInfrastructure/datasources/QueueSizeDataSource.py
@@ -16,16 +16,18 @@ from twisted.internet.defer import inlineCallbacks
 from zope.component import adapts, getUtility
 from zope.interface import implements
 
+from Products.ZenEvents import ZenEventClasses
+
 from ZenPacks.zenoss.PythonCollector.datasources.PythonDataSource import (
     PythonDataSource, PythonDataSourcePlugin, PythonDataSourceInfo,
     IPythonDataSourceInfo)
 
-from ZenPacks.zenoss.OpenStackInfrastructure.utils import add_local_lib_path
+from ZenPacks.zenoss.OpenStackInfrastructure.utils import add_local_lib_path, result_errmsg
 add_local_lib_path()
 
 from zenoss.protocols.interfaces import IAMQPConnectionInfo, IQueueSchema
 from zenoss.protocols.twisted.amqp import AMQPFactory
-
+import txamqp.client
 
 class QueueSizeDataSource(PythonDataSource):
     '''
@@ -69,6 +71,13 @@ class QueueSizeDataSourceInfo(PythonDataSourceInfo):
     testable = False
 
 
+# Persistent state
+# Technically, we could share the same client for all devices, but
+# in the interest of keeping things simple and consistent with the
+# other datasources..
+amqp_client = {}                     # amqp_client[device.id] = AMQClient object
+
+
 class QueueSizeDataSourcePlugin(PythonDataSourcePlugin):
     proxy_attributes = ()
 
@@ -99,21 +108,37 @@ class QueueSizeDataSourcePlugin(PythonDataSourcePlugin):
         self._amqpConnectionInfo = getUtility(IAMQPConnectionInfo)
         self._queueSchema = getUtility(IQueueSchema)
 
-        amqp = AMQPFactory(self._amqpConnectionInfo, self._queueSchema)
-        yield amqp._onConnectionMade
+        # reuse existing connectio if there is one.
+        if config.id in amqp_client and amqp_client[config.id]:
+            amqp = amqp_client[config.id]
+        else:
+            amqp = AMQPFactory(self._amqpConnectionInfo, self._queueSchema)
+            yield amqp._onConnectionMade
+            amqp_client[config.id] = amqp
 
         for queuename in ('$OpenStackInboundPerf', '$OpenStackInboundEvent',):
             queue = self._queueSchema.getQueue(queuename, replacements={'device': config.id})
-            info = yield amqp.channel.queue_declare(queue=queue.name,
-                                                    passive=True)
-
             try:
+                info = yield amqp.channel.queue_declare(queue=queue.name,
+                                                        passive=True)
                 results[queuename] = info.fields[1]
 
-            except IndexError:
+            except txamqp.client.Closed, e:
+                log.info("Unable to determine queue size for %s (queue does not exist)" % queue.name)
+                pass
+
+            except Exception, e:
+                log.info("Unable to determine queue size for %s (%s)" % (queue.name, e))
                 pass
 
         defer.returnValue(results)
+
+    def _disconnect_amqp_client(self, config_id):
+        if config_id in amqp_client and amqp_client[config_id]:
+            amqp = amqp_client[config_id]
+            amqp.disconnect()
+            amqp.shutdown()
+            del amqp_client[config_id]
 
     def onSuccess(self, result, config):
         data = self.new_data()
@@ -130,10 +155,44 @@ class QueueSizeDataSourcePlugin(PythonDataSourcePlugin):
                 continue
 
             if queue_map[point.id] not in result:
-                log.error("No queue count available for %s" % queue_map[point.id])
+                log.error("No queue count available for %s %s" % (config.id, queue_map[point.id]))
                 continue
 
             value = result[queue_map[point.id]]
             data['values'][ds0.component][point.id] = (value, 'N')
 
+        if len(data['values']):
+            data['events'].append({
+                'device': config.id,
+                'component': ds0.component,
+                'summary': 'OpenStack AMQP QueueSize: successful collection',
+                'severity': ZenEventClasses.Clear,
+                'eventKey': 'openstackCeilometerAMQPCollection',
+                'eventClassKey': 'QueueSizeSuccess',
+                })
+
         return data
+
+    def onError(self, result, config):
+        # just in case some error we don't recover from is occurring, throw
+        # out the connection.
+        self._disconnect_amqp_client(config.id)
+
+        errmsg = 'OpenStack AMQP QueueSize: %s' % result_errmsg(result)
+        log.error('%s: %s', config.id, errmsg)
+
+        data = self.new_data()
+        data['events'].append({
+            'device': config.id,
+            'summary': errmsg,
+            'severity': ZenEventClasses.Error,
+            'eventKey': 'openstackCeilometerAMQPCollection',
+            'eventClassKey': 'QueueSizeFailure',
+            })
+
+        return data
+
+    def cleanup(self, config):
+        log.info("Disconnecting any open AMQP connections for OpenStack AMQP QueueDataSource (%s)" % config.id)
+
+        self._disconnect_amqp_client(config.id)
