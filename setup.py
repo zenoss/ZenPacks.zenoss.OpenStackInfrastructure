597a0dd8aa501bbc0da358bc5456d8044e436497
yichi-lu <teegle@yahoo.com>
2015-08-21 14:37:31 -0500

    Start 2.1.2 development: Version 2.1.1 -> 2.1.2dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.OpenStackInfrastructure"
-VERSION = "2.1.1"
+VERSION = "2.1.2dev"
 AUTHOR = "Zenoss"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = [u'ZenPacks', u'ZenPacks.zenoss']
