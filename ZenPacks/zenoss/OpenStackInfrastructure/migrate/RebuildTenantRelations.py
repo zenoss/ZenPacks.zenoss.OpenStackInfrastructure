411ddfd8ee5674a687f8b2b80ee2b7776b4da2d7
Philip Carinhas <pcarinhas@zenoss.com>
2016-02-05 16:06:06 -0600

    Fixes ZEN-21930
    * Update the migration scripts (for relationships) to current number

--- ZenPacks/zenoss/OpenStackInfrastructure/migrate/RebuildTenantRelations.py
+++ ZenPacks/zenoss/OpenStackInfrastructure/migrate/RebuildTenantRelations.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (C) Zenoss, Inc. 2012, all rights reserved.
+# Copyright (C) Zenoss, Inc. 2012-2016, all rights reserved.
 #
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
@@ -45,7 +45,7 @@ class RebuildTenantRelations(ZenPackMigration):
 
     """
 
-    version = Version(2, 1, 0)
+    version = Version(2, 2, 0)
 
     def migrate(self, pack):
         results = ICatalogTool(pack.dmd.Devices).search(Tenant)
