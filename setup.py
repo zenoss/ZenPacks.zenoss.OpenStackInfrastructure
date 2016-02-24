0d7b7d88c160b6b6529265c05316cded916a3ec1
Philip Carinhas <pcarinhas@zenoss.com>
2016-02-24 16:57:49 -0600

    Start 2.1.3 Hotfix: Version 2.1.2 -> 2.1.3dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.OpenStackInfrastructure"
-VERSION = "2.1.2"
+VERSION = "2.1.3dev"
 AUTHOR = "Zenoss"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = [u'ZenPacks', u'ZenPacks.zenoss']
