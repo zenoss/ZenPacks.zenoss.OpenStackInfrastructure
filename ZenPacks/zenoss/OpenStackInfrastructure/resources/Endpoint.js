e1894f8a876a5944b0e8d4052c1e25da5e2389e8
Josh Wilmes <jwilmes@zenoss.com>
2016-06-01 15:47:27 +0000

    I can't find a reason for the workaround we were using- at least with ZPL's current entity link renderer, it seems to work fine.

--- ZenPacks/zenoss/OpenStackInfrastructure/resources/Endpoint.js
+++ ZenPacks/zenoss/OpenStackInfrastructure/resources/Endpoint.js
@@ -81,28 +81,6 @@ Ext.apply(Zenoss.render, {
         return value.toUpperCase();
     },
 
-    openstack_uid_renderer: function(uid, name) {
-        // Just straight up links to the object.
-        var parts;
-        if (!uid) {
-            return uid;
-        }
-        if (Ext.isObject(uid)) {
-            if (uid.uid && uid.uid.indexOf('/') > -1) {
-                parts = uid.uid.split('/');
-                name = parts[parts.length-1];
-            }
-            else {
-                name = uid.name;
-            }
-            uid = uid.uid;
-        }
-        if (!name) {
-            parts = uid.split('/');
-            name = parts[parts.length-1];
-        }
-        return Zenoss.render.link(null, uid, name);
-    },
 });
 
 
