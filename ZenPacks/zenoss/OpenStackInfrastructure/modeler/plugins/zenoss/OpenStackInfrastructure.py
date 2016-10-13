14f57d892628b09d1675e8a329efe1dd4131a020
yichi-lu <teegle@yahoo.com>
2016-10-13 03:12:17 -0500

    Fix a typo
    Fixes ZEN-25581

--- ZenPacks/zenoss/OpenStackInfrastructure/modeler/plugins/zenoss/OpenStackInfrastructure.py
+++ ZenPacks/zenoss/OpenStackInfrastructure/modeler/plugins/zenoss/OpenStackInfrastructure.py
@@ -375,7 +375,7 @@ class OpenStackInfrastructure(PythonPlugin):
                 agent['host'] = hostmap.get_hostid(agent['host'])
 
         for service in results['cinder_services']:
-            if 'host' in agent:
+            if 'host' in service:
                 service['host'] = hostmap.get_hostid(service['host'])
 
         results['zOpenStackNovaApiHosts'] = \
