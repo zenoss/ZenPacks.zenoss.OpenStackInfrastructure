43a172de123c7c8d1cca7a23f9a0951932e7a98e
Steve Paras-Charlton <stevepc@zenoss.com>
2016-11-01 11:24:33 -0700

    ZEN-25769: Create rabbitmq-ceilometer service definition ZEN-25769: add RabbitMQ-Ceilometer service definition on ZP upgrade if it is not already there ZEN-25769: refactor AMQP datasources and allow for collector-based rabbit

---
+++ ZenPacks/zenoss/OpenStackInfrastructure/libexec/init_rabbitmq-ceil.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+SENTINEL=/tmp/rabbit_ceil_ok
+if [ ! -e $SENTINEL ] ; then
+    USER=$1
+    PASSWORD=$2
+    #Tag with administrator in order to add ability to create and delete users, create and delete permissions through http api
+    rabbitmqctl add_user $USER $PASSWORD || true
+    rabbitmqctl change_password $USER $PASSWORD
+    rabbitmqctl set_user_tags $USER administrator management
+    rabbitmqctl add_vhost /zenoss || true
+    rabbitmqctl clear_permissions -p /zenoss $USER
+    rabbitmqctl set_permissions -p /zenoss $USER '.*' '.*' '.*'
+    rabbitmqadmin --port=45672 --vhost=/zenoss --username=$USER --password=$PASSWORD declare exchange name=zenoss.openstack.ceilometer type=topic
+    rabbitmqadmin --port=45672 --vhost=/zenoss --username=$USER --password=$PASSWORD declare exchange name=zenoss.openstack.heartbeats type=topic
+    rabbitmqctl delete_user guest || true
+    touch $SENTINEL
+fi
