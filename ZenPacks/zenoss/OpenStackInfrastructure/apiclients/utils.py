213e344fd88cc160c844a1052dc801d51bd57a6c
Josh Wilmes <josh-git@hitchhiker.org>
2017-12-12 14:28:23 -0500

    When using add_timeout with twisted.web._newclient, cancellation exceptions (#447)
    can be wrapped in ResponseNeverReceived exceptions.  If so, unwrap them
    so that the cancellation is processed correctly.

--- ZenPacks/zenoss/OpenStackInfrastructure/apiclients/utils.py
+++ ZenPacks/zenoss/OpenStackInfrastructure/apiclients/utils.py
@@ -17,6 +17,7 @@
 
 from twisted.internet import reactor, defer, error
 from twisted.python import failure
+from twisted.web._newclient import ResponseNeverReceived
 
 import logging
 LOG = logging.getLogger('zen.OpenStack.apiclient.utils')
@@ -49,6 +50,14 @@ def add_timeout(deferred, seconds, exception_class=error.TimeoutError):
     def handle_result(result):
         is_failure = isinstance(result, failure.Failure)
         is_cancelled = is_failure and isinstance(result.value, defer.CancelledError)
+
+        # With twisted.web._newclient, errors may be wrapped in a
+        # 'ResponseNeverReceived', including CancelledError.  If it is,
+        # set is_cancelled.
+        if is_failure and isinstance(result.value, ResponseNeverReceived):
+            if any([isinstance(x, failure.Failure) and isinstance(x.value, defer.CancelledError)
+                    for x in result.value.reasons]):
+                is_cancelled = True
 
         if delayed_timeout.active():
             # Cancel the timeout since a result came before it fired.
