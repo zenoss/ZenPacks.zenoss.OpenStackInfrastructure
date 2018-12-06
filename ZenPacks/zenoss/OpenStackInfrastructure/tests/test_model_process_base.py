8f9cdc25bd7f83dab7b24c20b33381e83fd57c1c
Josh Wilmes <jwilmes@zenoss.com>
2018-12-06 17:03:25 -0600

    Fix orgcomponent in test data

--- ZenPacks/zenoss/OpenStackInfrastructure/tests/test_model_process_base.py
+++ ZenPacks/zenoss/OpenStackInfrastructure/tests/test_model_process_base.py
@@ -398,7 +398,7 @@ class TestModelProcess(BaseTestCase):
             'hostname': 'liberty.zenoss.local',
             'id': 'host-liberty.zenoss.local',
             'modname': 'ZenPacks.zenoss.OpenStackInfrastructure.Host',
-            'set_orgComponent': 'zone-nova',
+            'set_orgComponent': 'zone-internal',
             'title': 'liberty.zenoss.local'})
 
         self.assertEquals(obj_map['host-overcloud-controller-1.localdomain'], {
