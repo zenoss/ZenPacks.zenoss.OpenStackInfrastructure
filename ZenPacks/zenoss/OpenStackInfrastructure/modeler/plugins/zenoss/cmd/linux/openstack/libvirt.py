c7276245f687beea637c303b8a16b6d51e7f7e51
Philip Carinhas <pac@fortuitous.com>
2019-02-20 18:14:45 -0600

    Make txsshclient loggers are INFO instead of debug. (#514)
    Fixes ZPS-5275
    
    * txsshclient uses DEBUG log level. Bump it down to INFO to avoid
      showing sensitive data.

--- ZenPacks/zenoss/OpenStackInfrastructure/modeler/plugins/zenoss/cmd/linux/openstack/libvirt.py
+++ ZenPacks/zenoss/OpenStackInfrastructure/modeler/plugins/zenoss/cmd/linux/openstack/libvirt.py
@@ -26,7 +26,7 @@ import logging
 from ZenPacks.zenoss.OpenStackInfrastructure.ssh import SSHClient
 
 ssh_logger = logging.getLogger('txsshclient')
-ssh_logger.setLevel(logging.DEBUG)
+ssh_logger.setLevel(logging.INFO)
 
 log = logging.getLogger('zen.OpenStack.libvirt')
 
