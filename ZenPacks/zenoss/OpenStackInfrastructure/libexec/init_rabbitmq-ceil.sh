9b74fe41122933fd2720592984bbd983f0e97a88
Josh Wilmes <jwilmes@zenoss.com>
2019-03-06 10:44:19 -0500

    Add time delay to rabbitmq-ceilometer initialization to avoid possible race condition

--- ZenPacks/zenoss/OpenStackInfrastructure/libexec/init_rabbitmq-ceil.sh
+++ ZenPacks/zenoss/OpenStackInfrastructure/libexec/init_rabbitmq-ceil.sh
@@ -3,6 +3,12 @@
 set -e
 SENTINEL=/tmp/rabbit_ceil_ok
 if [ ! -e $SENTINEL ] ; then
+
+    # allow time for rabbitmq to initialize the first time it is run.  Try
+    # to avoid triggering this bug in rabbitMQ prior to 3.4.0:
+    # https://github.com/rabbitmq/rabbitmq-server/commit/5567b10f6
+    sleep 45
+
     USER=$1
     PASSWORD=$2
     #Tag with administrator in order to add ability to create and delete users, create and delete permissions through http api
